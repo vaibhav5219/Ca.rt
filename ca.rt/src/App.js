999a5547c3c1329eaa3555c23152585d7a2c54dd
Vaibhav Kumar <vaibhavkumar5219@gmail.com>
2022-08-25 16:47:47 +0530

    cart hasbeing creating

--- ca.rt/src/App.js
+++ ca.rt/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/ListItem'
+import ListItem from './components/ListItem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <ListItem>{/*  //all custom components start with Capital letters */}
+          {/*  Can't use xml in this area -  Hello List */}
+      </ListItem>
+
     </div>
   );
 }
---
+++ ca.rt/src/components/ListItem.js
@@ -0,0 +1,16 @@
+const ListItem = () =>{
+    return 
+    (
+        <div>
+            <img src="/assets/cart.png" alt="some title"></img>\
+            <div>
+                <div>
+                    <span>$340</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default ListItem;
