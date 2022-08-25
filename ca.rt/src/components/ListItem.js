066f2c1f1855c7b96461d0ea612cd1459258606b
Vaibhav Kumar <vaibhavkumar5219@gmail.com>
2022-08-25 18:00:47 +0530

    Card item added in itemcomponent

--- ca.rt/src/components/ListItem.js
+++ ca.rt/src/components/ListItem.js
@@ -1,15 +1,26 @@
+import AddToCartIcon from "../assets/icons/cart.png"
+
 const ListItem = () =>{
-    return 
-    (
-        <div>
-            <img src="/assets/cart.png" alt="some title"></img>\
+    return (
+        <div className={"item-card"}>
+            <img src="/assets/cart.png" alt="some title" ></img>
             <div>
                 <div>
-                    <span>$340</span>
+                    <span>₹340</span>
+                    <small>
+                        <strike>₹450</strike>
+                    </small>
+                </div>
+                <div className={"title"}>
+                    <h3>Title of the List Item</h3>
                 </div>
             </div>
+            <button className={"cart-add"}>
+                <span>Add to cart</span>
+                <img src={AddToCartIcon} alt="Cart Icon" width={"20"} height={"20"}></img>
+            </button>
         </div>
-    )
+    );
 }
 
 
