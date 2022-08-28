051c04b0680b6cffec12e8edeaf118932bb87258
Vaibhav <vaibhavkumar5219@gmail.com>
2022-08-28 11:01:30 +0530

    Use State Hook used for adding cart event listener

--- ca.rt/src/components/Layouts/SubHeader.js
+++ ca.rt/src/components/Layouts/SubHeader.js
@@ -1,11 +1,11 @@
 import "../../styles/SubHeader.scss"
 const SubHeader = () => {
     return (
-        <div class="d-flex bd-highlight mb-3">
-            <div class="p-2 bd-highlight">Children Category </div>
-            <div class="p-2 bd-highlight">Men Category </div>
-            <div class="p-2 bd-highlight">Women Category </div>
-            <div class="p-2 bd-highlight">Offer Zone</div>
+        <div className="d-flex bd-highlight mb-3">
+            <div className="p-2 bd-highlight">Children Category </div>
+            <div className="p-2 bd-highlight">Men Category </div>
+            <div className="p-2 bd-highlight">Women Category </div>
+            <div className="p-2 bd-highlight">Offer Zone</div>
         </div>
     );
 }
--- ca.rt/src/components/ListItems/ListItem.js
+++ ca.rt/src/components/ListItems/ListItem.js
@@ -1,8 +1,16 @@
 //import AddToCartIcon from "../assets/icons/cart.png"
 import AddToCartIcon from "../../assets/icons/cart.png"
+import {useState} from "react"
 
 const ListItem = ({data}) =>{
+    //let[message,setMessage] = useState("Not added to cart yet");
+    //let message = "Not added to cart yet"
+    const [counter, setCounter] = useState(0);
+
+    //const increaseCounterByOne = ()=> { setCounter: counter+1  };
+    //const decreaseCounterByOne = ()=> { this.setCounter= this.state.counter-1; };
     
+
     return (
         <div className={"item-card"}>
             <img className={"img-fluid"} src={`/assets/${data.thumbnail}`}  alt="some title" ></img> {/*  src={"/assets/"+data.thumbnail} */}
@@ -17,10 +25,16 @@ const ListItem = ({data}) =>{
                     <h3>{data.title}</h3>
                 </div>
             </div>
-            <button className={"cart-add "} >
+            {/* <small className={"class-message"}>{message}</small>
+            <button className={"cart-add "} onClick={handelClick} >
                 <span className={"cart-add "}>Add to cart</span>
                 <img src={AddToCartIcon} alt="Cart Icon" width={"20"} height={"20"}></img>
-            </button>
+            </button> */}
+            <div className={"cart-addon cart-add"}  >
+                <button onClick={() => setCounter( counter>0 ? counter - 1 :counter)}><span>-</span></button>
+                <span className={"counter"}>{counter} </span>
+                <button onClick={() => setCounter(counter + 1)}><span>+</span></button>
+            </div>
         </div>
     ); 
 }
