32cf94e6637b8506d658536f595996b654c70d1f
Vaibhav Kumar <vaibhavkumar5219@gmail.com>
2022-09-07 16:58:19 +0530

    CleanUp Function used

--- ca.rt/src/components/Timer/Timer.js
+++ ca.rt/src/components/Timer/Timer.js
@@ -4,10 +4,15 @@ const Timer = () => {
     const [counter, setCounter] = useState(0);
 
     useEffect(()=>{
-        setInterval(()=>{
-            setCounter(counter => counter+1);
+        const timeInterval = setInterval(()=>{
+            setCounter(previousValueOfCounter => previousValueOfCounter+1);
         }, 1000);
-    },[]);
+
+        return () => {  // It will execute 1st re-render 
+            console.log("Timer Component Rerendered");
+            clearInterval(timeInterval);    // If we will not use memory leak will happen
+        }
+    },[]);    //   [counter]
 
     return (
         <div >
