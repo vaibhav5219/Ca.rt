757d61be7e3a309c4a58dc252326f4adf2676642
Vaibhav <vaibhavkumar5219@gmail.com>
2022-09-10 22:15:46 +0530

    firebase api call using fetch

--- ca.rt/src/components/Products/Products.js
+++ ca.rt/src/components/Products/Products.js
@@ -1,9 +1,8 @@
 import ListItem from "../ListItems/ListItem";
-import { useState } from "react";
-import Form from "../Form/Form";
+import { useEffect, useState } from "react";
 
 const Products = () => {
-    const [item, setItems] = useState([
+    const [items, setItems] = useState([
         {
             id: 0,
             discountedPrice: 340,
@@ -19,37 +18,27 @@ const Products = () => {
             thumbnail: "cart.png"
         }
     ]);
+    useEffect(()=>{  // 1st parm is function, which executes 1st render and re-render
+        const res = fetch('https://react-cart-api-2022-default-rtdb.firebaseio.com/Items.json')
+        .then(response => response.json())
+        .then(data => {                         // To avoid call back hell, so use chaining
+            console.log(data)
+        })
+        .catch(error =>{      // Error handling
+            console.log(error);
+        })
+    },[]);
 
-    const submitForm = event => {
-        event.preventDefault();
-
-        if (item.discountedPrice > item.price) {
-            alert("Discounted price is not greater than actual price");
-            return;
-        }
-        // Communicate via child to parent =>    LIFTING THE STATE UP
-        console.log("Item Updated! ", item);
-    }
-    const handleInput = event => {
-        console.log(event.target.value, event.target.name);
-        setItems({
-            ...item,   // sperad operator
-            [event.target.name]: event.target.value
-        });
-    }
     return (
         <div>
             <div className="d-flex flex-column"></div>
             <div className={"product-list d-flex flex-column "}>
                 <div className={"product-list-wrapper  d-flex justify-content-center"}>
-                    {/* <ListItem data={item}></ListItem> */}
                     {
-                        item.map(it => {
-                            console.log(it);
-                            return (<ListItem data={it} key={it.id} />);
+                        items.map(item => {
+                            return (<ListItem data={item} key={item.id} />);
                         })
                     }
-                    {/* {[<ListItem data={item[0]}/>, <ListItem data={item[1]}/>, <ListItem data={item[2]}/>, ]} */}
                 </div>
             </div >
             <div className="d-flex flex-column"></div>
