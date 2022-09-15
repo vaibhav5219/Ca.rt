d3d9ff857332f59027d34918b0f47cd6d3df3b31
Vaibhav Kumar <vaibhavkumar5219@gmail.com>
2022-09-15 17:00:20 +0530

    Loader using React portal using React dom

--- ca.rt/src/components/UI/Loader.js
+++ ca.rt/src/components/UI/Loader.js
@@ -1,11 +1,27 @@
+import ReactDOM from "react-dom";
+
 const Loader = () => {
-    return (<>
-        <div className="loader-overlay"></div>
-        <div className="loading-dots">
-            <div>loading...</div>
-            <div className="loading-dots--dots"></div>
-        </div>
-    </>
+    // return (<>
+    //     <div className="loader-overlay"></div>
+    //     <div className="loading-dots">
+    //         <div>loading...</div>
+    //         <div className="loading-dots--dots"></div>
+    //     </div>
+    // </>
+    // )
+
+    return (
+        ReactDOM.createPortal(
+            <>
+                <div className="loader-overlay"></div>
+                <div className="loading-dots">
+                    <div>loading...</div>
+                    <div className="loading-dots--dots"></div>
+                </div>
+            </>
+            ,
+            document.getElementById("loader-root")
+        )
     )
 }
 
