dcdde9eed47e00ff7141b758db8deb34f8042194
Vaibhav Kumar <vaibhavkumar5219@gmail.com>
2023-02-01 12:53:42 +0530

    navigation using hook - useNavigate , uselocation

--- ca.rt/src/components/Products/ProductItem.js
+++ ca.rt/src/components/Products/ProductItem.js
@@ -1,6 +1,23 @@
+import { useNavigate, useLocation, withRouter  } from "react-router-dom";
+import {Navigate} from "react-router"; // not working
+
 const ProductItem = () => {
+    const history = useNavigate();
+    const location = useLocation();
+
+
+    const handleRedirection = () => {
+        console.log(location);
+        //history(-1);
+        //history(2);
+        
+    }
+
     return (
+        <>
         <h1>Product Item Component</h1>
+        <button onClick={handleRedirection}>Go back to Product page</button>
+        </>
     )
 }
 
