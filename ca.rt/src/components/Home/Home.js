ad1a1d329ec1990492150d11b310db7f3e917839
Vaibhav Kumar <vaibhavkumar5219@gmail.com>
2023-02-08 16:34:29 +0530

    Adding QueryString in Routes Programmatically

--- ca.rt/src/components/Home/Home.js
+++ ca.rt/src/components/Home/Home.js
@@ -1,12 +1,34 @@
 import { Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Home = () =>{
+
+    const handleRedirection = () => {
+
+    }
+
     return (
         <Fragment>
             <h1>Default Message</h1>
             {/* <a href='/products'>Product</a> */}
-            <Link to={"/products"}>Product via Link </Link>
+            <ul>
+                <li><Link to={"/products"}>Product via Link </Link></li>
+                <li><Link to={"/products?search=something"}>Product via Link </Link></li>
+                <li><Link to={{
+                    Pathname: '/products',
+                    search: 'search=mobile'
+                }}>Product via Mobile </Link>
+                </li>
+                <li><NavLink to={{
+                    Pathname: '/products',
+                    search: 'search=usingnavLink'
+                }}>Product with Query String </NavLink>
+                </li>
+                <li>
+                    <button onClick={handleRedirection}>Product Redirection using Button</button>
+                </li>
+            </ul>
+            
         </Fragment>
     )
 }
